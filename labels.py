c2c695c65e3d40e2dd67c8b942bb5117a22bdc7c
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-12-11 13:12:51 -0500

    commiting my work before lunch - almost done w/ first pass tokenization implementation

--- labels.py
+++ labels.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import argparse
+import re
 
 # These are all present flairs from the subreddit
 math_sci = ['Computer Science', 'Mathematics & Statistics', 'Software Engineering',
@@ -28,25 +29,45 @@ agriculture_environment = ['Environment', 'Agricultural & Environmental Science'
                            'Plant Science', 'Geography', 'Urban Planning']
 law = ['Law']
 
-# TODO: programmatically add variations here; science -> sci, engineering -> eng
-label_to_flairs = {'Math Sciences': math_sci,
+# TODO: keep going with the adding of different stems/acronyms
+label_to_flairs = {'Math Sciences': math_sci + ['Mathematics', 'Math'],
                    'Physical Sciences': physical_sci,
                    'Life Sciences': life_sci,
                    'Management': management,
-                   'Medicine': medicine,
+                   'Medicine': medicine + ['Med'],
                    'Music': music,
                    'Education': education,
                    'Humanities': humanities,
-                   'Social Sciences': social_science,
-                   'Agriculture and Environment': agriculture_environment,
+                   'Social Sciences': social_science + ['Poli Science', 'Soci', 'IDS', 'Econ'],
+                   'Agriculture and Environment': agriculture_environment + ['Enviro'],
                    'Law': law}
+for flairs in label_to_flairs.values():
+    for flair in flairs:
+        if 'Science' in flair:
+            flairs.append(flair.replace('Science', 'Sci'))
+        if 'Engineering' in flair:
+            flairs.append(flair.replace('Engineering', 'Eng'))
+        if 'Computer' in flair:
+            flairs.append(flair.replace('Computer', 'Comp'))
 
-# TODO: figure out a way to add stems/acryonyms such as CS, poli, econ, IDS, etc. May require tokenization
+strs_to_replace = ['honors', 'honours', 'joint']
+regexes_to_replace = [r'U\d', r'\d{4}', r'\'\d{2}', r'B.?((Sc)|A|(Eng)|(Ed)|(Mus)|(Comm)).?']
+def tokenize_flair_into_possible_majors(flair: str):
+    flair = flair.lower()
+    for str_to_repalce in strs_to_replace:
+        flair = flair.replace(str_to_repalce, '')
+    for regex_to_replace in regexes_to_replace:
+        flair = re.sub(regex_to_replace, '', flair)
+    tokens = [flair]
+    # TODO: if there's a valid split on & or /, append all elements of the result to tokens
+    # then, we will loop over and check all of the values of tokens against the values of label_to_flairs
+
 
 def flair_to_label(row):
-    flair = row['flair']
+    flair = tokenize_flair_into_possible_majors(row['flair'])
     for label, possible_flairs in label_to_flairs.items():
-        if any(possible_flair.lower() in flair.lower() for possible_flair in possible_flairs):
+        # TODO: for efficieny move the lower()ing outside of the outer loop somehow
+        if flair.strip() in [possible_flair.lower() for possible_flair in possible_flairs]:
             return label
     return 'UNKNOWN'
 
