db930713d3b9866749538fec04e2ee83a5ed09c2
mchacks-github-demo <46543122+chenxuan-zhou@users.noreply.github.com>
2021-12-13 14:43:50 -0500

    update requirements; add sentence-BERT (need debug)

--- src/clustering/clustering.py
+++ src/clustering/clustering.py
@@ -1,12 +1,17 @@
 import numpy as np
 import pandas as pd
-import spacy_universal_sentence_encoder
+import spacy_universal_sentence_encoder, spacy_sentence_bert
 from sklearn.cluster import KMeans
 
-# https://spacy.io/universe/project/spacy-universal-sentence-encoder
-nlp = spacy_universal_sentence_encoder.load_model('en_use_lg')
+sBERT = False
 
-# https://spacy.io/universe/project/spacy-sentence-bert
+if not sBERT:
+    # https://spacy.io/universe/project/spacy-universal-sentence-encoder
+    nlp = spacy_universal_sentence_encoder.load_model('en_use_lg')
+else:
+    # TODO: debug sentence-BERT usage
+    # https://spacy.io/universe/project/spacy-sentence-bert
+    nlp = spacy_sentence_bert.load_model('en_stsb_roberta_base')
 
 # read comments, vectorization 
 # TODO: this is contextualized embedding, need n-gram vectorization too
